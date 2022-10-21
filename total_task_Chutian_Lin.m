98cb9471090ef2ace65aaed6cf345ea4e0b25469
ChutianLin <114811141+ChutianLin@users.noreply.github.com>
2022-10-21 14:08:27 +0100

    Update total_task_Chutian_Lin.m

--- total_task_Chutian_Lin.m
+++ total_task_Chutian_Lin.m
@@ -315,8 +315,8 @@ ind1_B=ceil(201*2400/Fs);
 ind2_B=ceil(201*4000/Fs);
 
 for j=1:ind_s
-    value1(j,1)=0;
-    value1(j,2)=0;%for frequency region B
+    xxx(j,1)=0;
+    xxx(j,2)=0;%for frequency region B
     %intercept the Orig_s according to the time index, and store 
     %the result in segOrig_phS
     segOrig_phS(j,1:201)=(Orig_s{j}(indexSegStart_s(j):indexSegEnd_s(j),1))';
@@ -324,32 +324,32 @@ for j=1:ind_s
     %the |X(k)|^2 of the number of columns 11 to 41 of 
     %each row is added and stored into the first column of value1
     for m=ind1_A:ind2_A
-        value1(j,1)=abs(segOrig_phS_ft(j,m))^2+value1(j,1);
+        xxx(j,1)=abs(segOrig_phS_ft(j,m))^2+xxx(j,1);
     end
-    aveEn_dB_A_s(j,1)=10*log10(value1(j,1)/(ind2_A-ind1_A+1));
+    aveEn_dB_A_s(j,1)=10*log10(xxx(j,1)/(ind2_A-ind1_A+1));
     enOrigRegAB_phS(j,1)=aveEn_dB_A_s(j,1);
     for n=ind1_B:ind2_B
-        value1(j,2)=abs(segOrig_phS_ft(j,n))^2+value1(j,2);
+        xxx(j,2)=abs(segOrig_phS_ft(j,n))^2+xxx(j,2);
     end
-    aveEn_dB_B_s(j,1)=10*log10(value1(j,2)/(ind2_B-ind1_B+1));
+    aveEn_dB_B_s(j,1)=10*log10(xxx(j,2)/(ind2_B-ind1_B+1));
     enOrigRegAB_phS(j,2)=aveEn_dB_B_s(j,1);
 end
 
 for k=1:ind_aa
-    value2(k,1)=0;
-    value2(k,2)=0;
+    yyy(k,1)=0;
+    yyy(k,2)=0;
     segOrig_phAA(k,1:201)=(Orig_aa{k}(indexSegStart_aa(k):indexSegEnd_aa(k),1))';
     segOrig_phAA(k,1:201)=(Orig_aa{k}(indexSegStart_aa(k):indexSegEnd_aa(k),1))';
     segOrig_phAA_ft(k,1:201)=abs(fft(segOrig_phAA(k,1:201)));
     for o=ind1_A:ind2_A
-        value2(k,1)=abs(segOrig_phAA_ft(k,o))^2+value2(k,1);
+        yyy(k,1)=abs(segOrig_phAA_ft(k,o))^2+yyy(k,1);
     end
-    aveEn_dB_A_aa(k,1)=10*log10(value2(k,1)/(ind2_A-ind1_A+1));
+    aveEn_dB_A_aa(k,1)=10*log10(yyy(k,1)/(ind2_A-ind1_A+1));
     enOrigRegAB_phAA(k,1)=aveEn_dB_A_aa(k,1);
     for p=ind1_B:ind2_B
-        value2(k,2)=abs(segOrig_phAA_ft(k,p))^2+value2(k,2);
+        yyy(k,2)=abs(segOrig_phAA_ft(k,p))^2+yyy(k,2);
     end
-    aveEn_dB_B_aa(k,1)=10*log10(value2(k,2)/(ind2_B-ind1_B+1));
+    aveEn_dB_B_aa(k,1)=10*log10(yyy(k,2)/(ind2_B-ind1_B+1));
     enOrigRegAB_phAA(k,2)=aveEn_dB_B_aa(k,1);
 end
 
@@ -424,9 +424,9 @@ gPDF_aa(:,2)=(gPDF_B_aa(1:64))';
 
 % Making a table with the table function
 item={'Region A of aa';'Region B of aa';'Region A of s';'Region B of s'};
-mean={m_A_aa;m_B_aa;m_A_s;m_B_s};
-variance={v_A_aa;v_B_aa;v_A_s;v_B_s};
-table_para=table(item,mean,variance);
+meannn={m_A_aa;m_B_aa;m_A_s;m_B_s};
+varianceee={v_A_aa;v_B_aa;v_A_s;v_B_s};
+table_para=table(item,meannn,varianceee);
 
 save('table with modelling of the energy values.mat','gPDF_aa','gPDF_s');
 load ('table with modelling of the energy values.mat');
